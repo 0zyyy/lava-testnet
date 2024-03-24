542623f494e58c42791ec86fa945e09fd3cc1b80
Jalu Fazlurrahman <77714640+0zyyy@users.noreply.github.com>
2024-03-24 12:40:59 +0000

    (fix): rpc link

--- ethereum.sh
+++ ethereum.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-RPC_URL="https://eth1.lava.build/lava-referer-8b51600b-b3cc-4893-89ec-5de41f0724db/"
+RPC_URL="https://eth1.lava.build/lava-referer-342587b9-4ed0-4c5e-a696-291e1459d383/"
 
 OUTPUT_DIR="logs"
 
